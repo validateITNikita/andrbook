1a7e9e5fd29719d84682c0f1cc328146ca83e49d
validateITNikita <idshnicknickita@gmail.com>
2024-01-20 13:21:46 +0600

    refactor: home page card gap

--- components/Start.vue
+++ components/Start.vue
@@ -14,15 +14,15 @@ import { formatEther, formatUnits } from 'ethers'
         </div>
 
         <div
-            class="w-[500px] h-fit text-white absolute right-40 my-auto top-0 bottom-0 right-0 bg-gradient-to-b from-transparent to-white/10 backdrop-blur-xl py-6 px-10 rounded-lg border border-solid border-light/20 text-balance leading-normal space-y-2 flex flex-col">
-            <p class="font-black text-4xl">The Gentlemen NFT</p>
+            class="w-[500px] h-fit text-white absolute right-40 my-auto top-0 bottom-0 right-0 bg-gradient-to-b from-transparent to-white/10 backdrop-blur-xl py-6 px-10 rounded-lg border border-solid border-light/20 text-balance leading-normal space-y-2 flex flex-col gap-y-4">
+            <p class="font-black text-5xl">Gentlemen NFT</p>
             <p class="opacity-75 text-lg">
                 Stands out as a groundbreaking opportunity for enthusiasts and investors alike to
                 delve into the world of finance through exclusive educational content. These non-fungible tokens represent more
                 than just digital assets. 
                 They unlock a wealth of knowledge curated by experts in the field.
             </p>
-            <button class="border border-solid border-light/20 rounded-lg px-10 py-2 w-fit mx-auto  bg-white/10 backdrop-blur-xl hover:bg-white shadow active:shadow-xl active:shadow-white/40 duration-100 text-white hover:text-black active:scale-90 cursor-pointer font-bold text-xl">
+            <button class="border border-solid border-light/20 rounded-lg px-10 py-2 w-fit mr-auto bg-white/10 backdrop-blur-xl hover:bg-white shadow active:shadow-xl active:shadow-white/40 duration-100 text-white hover:text-black active:scale-90 cursor-pointer font-bold text-xl">
                 MINT
             </button>
         </div>
